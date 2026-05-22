f19218f7267e7fdb478763be53cf8f6097b532a8
Roy Quader <35521+roydq@users.noreply.github.com>
2026-05-22 14:41:33 -0400

    Enabling sum stuff

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -5,4 +5,48 @@
 
 ---@module 'lazy'
 ---@type LazySpec
-return {}
+return {
+  { -- Enhanced commenting with treesitter support
+    'numToStr/Comment.nvim',
+    opts = {
+      -- Add any options here
+      -- For example:
+      -- padding = true,
+      -- sticky = true,
+      -- ignore = '^$', -- ignore empty lines
+    },
+    lazy = false,
+    keys = {
+      { 'gcc', mode = 'n', desc = 'Comment toggle current line' },
+      { 'gc', mode = { 'n', 'o' }, desc = 'Comment toggle linewise' },
+      { 'gc', mode = 'x', desc = 'Comment toggle linewise (visual)' },
+      { 'gbc', mode = 'n', desc = 'Comment toggle current block' },
+      { 'gb', mode = { 'n', 'o' }, desc = 'Comment toggle blockwise' },
+      { 'gb', mode = 'x', desc = 'Comment toggle blockwise (visual)' },
+    },
+  },
+
+  { -- Delete buffers without closing windows
+    'echasnovski/mini.bufremove',
+    version = '*',
+    config = function()
+      require('mini.bufremove').setup()
+    end,
+    keys = {
+      {
+        '<leader>bd',
+        function()
+          require('mini.bufremove').delete(0, false)
+        end,
+        desc = '[B]uffer [D]elete (keep window)',
+      },
+      {
+        '<leader>bD',
+        function()
+          require('mini.bufremove').delete(0, true)
+        end,
+        desc = '[B]uffer [D]elete (force)',
+      },
+    },
+  },
+}
